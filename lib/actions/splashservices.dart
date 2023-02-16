26c41763cfce1960ec027d0239632c6bbf19fb8c
Purnamita Baral <purnamitabaral@gmail.com>
2023-02-16 23:58:32 +0530

    third commit

--- lib/actions/splashservices.dart
+++ lib/actions/splashservices.dart
@@ -1,15 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:organ_bridge_project/forms/certificate_generate.dart';
 import 'package:organ_bridge_project/homepage/home_page.dart';
 //import 'package:organ_bridge/pages/homepage.dart';
 
 class SplashServices {
   void display(BuildContext context) {
-    
     Timer(
         const Duration(seconds: 5),
-        () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => HomePage())));
+        () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => CertificateGenerate())));
   }
 }
